39e34891a6f123a2ca3d58d87f11cd627400b902
Cihan Kara <cihankara632@gmail.com>
2024-09-09 21:00:03 +0300

    api gateway modülü eklendi yöndendirilme başarı ile eklendi

---
+++ gateway-server/src/test/java/com/finkoto/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finkoto.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
